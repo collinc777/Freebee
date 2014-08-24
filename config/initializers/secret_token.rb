0eaeac1b5f1129b36efe03abc557fde825ac2c48
System Administrator <root@wireless-10-145-154-18.public.utexas.edu>
2014-08-23 19:10:23 -0500

    tcarona - mobile scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreeBee::Application.config.secret_token = 'd52f15f844c29193a9f383f1c0b04c94dea3e1709800223e2ac2fc69a2da7266ae7259dc74fa93a29f78882a77551c3df2990b705ec87351e7343d2dbcf8bc9d'
